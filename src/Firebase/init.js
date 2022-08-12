42b878060d8dc79f3667e660b723de24bc24b674
kevinwiding <kevinwiding1@gmail.com>
2022-08-12 19:52:35 +0200

    testing

--- src/Firebase/init.js
+++ src/Firebase/init.js
@@ -3,7 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
